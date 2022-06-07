7700ca297b36b6a009ebad718e90e387d8c2147c
Anton Kotov <kotov-anton@yandex.ru>
2022-06-07 09:42:43 +0300

    added test results

--- jmeter/run-jmeter.sh
+++ jmeter/run-jmeter.sh
@@ -1,1 +1,1 @@
-jmeter -n -t 'Test Plan.jmx' -l ./webflux-jdbc-5000-10-100-0.jtl  -e -o /Users/anton/dev/jmeter/webflux-jdbc-5000-10-100-0
+jmeter -n -t 'Test Plan.jmx' -l ./webflux-jdbc-5000-10-100-0.jtl  -e -o ./webflux-jdbc-5000-10-100-0
